76d464f8c93a8a1e663ba614eaabe191b5bc94b4
wibo <william.boquist@gmail.com>
2017-08-25 08:29:22 +0200

    TEST7

--- Group6Chat/Group6Chat/PrivateChatForm.cs
+++ Group6Chat/Group6Chat/PrivateChatForm.cs
@@ -24,6 +24,7 @@ namespace Group6Chat
             ReceiverOfPrivateMessage = receiver;
             SenderOfPrivateMessage = sender;
             HostServer = hostServer;
+            this.ActiveControl = textBoxPrivateInput;
             InitializeComponent();
         }
 
